5f10e516eb181757cccf3eced0129631cc217cd5
olamide-thinker <olamide.thinker@gmail.com>
2025-04-24 11:32:53 +0100

    s

--- components/pages/home/Testimonials.tsx
+++ components/pages/home/Testimonials.tsx
@@ -5,6 +5,37 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+const testimonials = [
+  {
+    imageSrc: "/assets/leadership/placeholder_user.png",
+    testimonialText:
+      "My aunty I'm not sleeping on this medicinal spice!!! I have been having sleepless nights for weeks until recently when I noticed I doze off almost immediately when I hit my bed. This happened to me once, when you gave me to try at your place, I was like maybe because I am tired, but omo, it happened again o, the way my sleep has been for 3 days now eh, is surprising. I am like, what did I add or change lately? Then, I noticed that spice I got was the only thing new I added to my daily lifestyle. I have only used it twice in my meal oo. Now imagine if it's used every-time I cook... Interesting, right? I am shocked my sis😳😳. This product works. Thank you 🙏",
+    name: "Stella Fadeyibi",
+    // role: "Wellness Enthusiast",
+  },
+  {
+    imageSrc: "/assets/leadership/placeholder_user.png",
+    testimonialText:
+      "Sis, I have this skin irritation on my skin, this has been disturbing me more often. I use on, and it went but still comes back, this last time I have it again, then I decided to try the ointment I got from your company that you say works on the skin, be it arthritis, skin irritation burn, bites and so on, I'm like let me try it ooo... Sis see now the second picture I sent now, it's totally drying up and has started getting dry unlike the ORI that takes time. Omo, I am happy oo, I just pray it does not come back again. Anyways am like I can't keep this to myself, I must share this testimony about this your products ooo.",
+    name: "Aderonke Temitayo",
+    // role: "Frequent Traveler",
+  },
+  {
+    imageSrc: "/assets/leadership/placeholder_user.png",
+    testimonialText:
+      "“Nice tea I received my tea last week and I love it, I woke up feeling light and smart 🥰”",
+    name: "Mrs. Lawal Ifeoluwa",
+    // role: "Health Coach",
+  },
+  {
+    imageSrc: "/assets/leadership/placeholder_user.png",
+    testimonialText:
+      "Yes I do, Thanks. The bleeding stopped on friday. Saturday and Sunday I was draining. But thank God by Monday the draining stoped. The new thing I just observed for some days now is I just lost appetite which is a big corcerned for me. Aside that am doing Good and everyone is testifying that the medication is really working.",
+    name: "Mrs Debby O",
+    // role: "Nature Lover",
+  },
+];
+
 export const Testimonials = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -41,14 +72,19 @@ export const Testimonials = () => {
 
       {/* Slider Component */}
       <Slider {...settings}>
-        {Array.from({ length: 4 }).map((_, i) => (
+        {testimonials.map((a, i) => (
           <div
             key={i}
             className={`relative transition-all py-8 duration-300 transform ${
               currentIndex === i ? "scale-100" : "scale-60 blur-md"
             } ease-in-out`}
           >
-            <TestimonialCard />
+            <TestimonialCard
+              imageSrc={"/assets/leadership/placeholder_user.png"}
+              testimonialText={a.testimonialText}
+              name={a.name}
+              // role={a.role}
+            />
           </div>
         ))}
       </Slider>
--- components/sharedUi/TestimonialCard.tsx
+++ components/sharedUi/TestimonialCard.tsx
@@ -1,21 +1,41 @@
 import React from "react";
 import Image from "next/image";
 
-export const TestimonialCard = () => {
+type props = {
+  imageSrc: string;
+  testimonialText: string;
+  name: string;
+  // role: string;
+};
+
+export const TestimonialCard = ({
+  imageSrc,
+  testimonialText,
+  name,
+}: // role,
+props) => {
   return (
     <div className="relative flex flex-col items-center w-full max-w-lg p-6 mx-auto transition-all duration-300 ease-in-out bg-white rounded-3xl hover:shadow-lg hover:scale-105">
       {/* Testimonial Content */}
       <p className="mb-6 text-lg leading-relaxed text-center text-gray-800">
-        “Had the most amazing experience at Fumzy Wellness Breakaway Resort! The
+        <span className="p-1 mr-2 text-2xl font-bold rounded bg-muted">
+          &quot;
+        </span>
+        {testimonialText}{" "}
+        <span className="p-1 ml-2 text-2xl font-bold rounded bg-muted">
+          &quot;
+        </span>
+        {/* “Had the most amazing experience at Fumzy Wellness Breakaway Resort! The
         serene environment, top-notch facilities, and exceptional service made
         my stay unforgettable. I came for a relaxing getaway and left feeling
-        rejuvenated and refreshed.”
+        rejuvenated and refreshed.” */}
       </p>
 
       {/* Profile Image */}
       <div className="mt-4">
         <Image
-          src="/assets/home/Biologically_targeted_Herbal_mixtures.jpg"
+          // src="/assets/home/Biologically_targeted_Herbal_mixtures.jpg"
+          src={imageSrc}
           width={80}
           height={80}
           alt="Profile Image"
@@ -25,8 +45,8 @@ export const TestimonialCard = () => {
 
       {/* Name & Role */}
       <div className="mt-4 text-center">
-        <p className="text-xl font-semibold text-gray-800">Rosa James</p>
-        <p className="text-sm text-gray-500">Wellness Enthusiast</p>
+        <p className="text-xl font-semibold text-gray-800">{name}</p>
+        {/* <p className="text-sm text-gray-500">{role}</p> */}
       </div>
     </div>
   );
